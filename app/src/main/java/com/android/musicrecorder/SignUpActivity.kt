ccbcb4cc343973689bff9725aca75cfc631f43d0
Maciej Kutrowski <maciej.michal.kutrowski@gmail.com>
2020-01-06 22:56:54 +0100

    z domu

--- app/src/main/java/com/android/musicrecorder/SignUpActivity.kt
+++ app/src/main/java/com/android/musicrecorder/SignUpActivity.kt
@@ -1,10 +1,12 @@
 package com.android.musicrecorder
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
@@ -24,8 +26,23 @@ class SignUpActivity : AppCompatActivity() {
         password = findViewById(R.id.editTextPasswdId)
         signUpBtn = findViewById(R.id.signUpBtnId)
         signUpBtn.setOnClickListener(View.OnClickListener {
+            val emailString: String = email.text.toString()
+            val passwordString: String = password.text.toString()
+            email
+            if (email.text.isEmpty()){
+                email.setText("Enter Email!")
+            }
+            else if(password.text.isEmpty()){
+                password.setText("Enter password")
+            }
+            else if(password.text.isEmpty() and email.text.isEmpty()){
+                firebaseAuth
+                    .createUserWithEmailAndPassword(emailString, passwordString)
+                    .addOnCompleteListener(this@SignUpActivity, ) /
+//https://www.youtube.com/watch?v=V0ZrnL-i77Q&t=600s
+            }
 
-        })
     }
+}
 }
 
